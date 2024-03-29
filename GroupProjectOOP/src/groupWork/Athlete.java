02117ea23c877383ec0813eb7d9efe54a8ede10a
oc <oconnorburton387@gmail.com>
2024-03-29 13:44:43 -0500

    Create Athlete.java

---
+++ GroupProjectOOP/src/groupWork/Athlete.java
@@ -0,0 +1,20 @@
+package groupWork;
+
+public class Athlete {
+	private int trn;
+	private String fName;
+	private String mName;
+	private String lName;
+	private char gender;
+	private Date DOB;
+	private float height;
+	private float weight;
+	private String nationality;
+	private boolean affiliated;
+	
+	public void AddAthlete()
+	{
+		
+	}
+
+}
