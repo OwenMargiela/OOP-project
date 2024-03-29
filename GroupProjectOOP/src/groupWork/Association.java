de185d3384e3fdcfe21d1b27b10fc0e55d0bd21e
oc <oconnorburton387@gmail.com>
2024-03-29 11:06:35 -0500

    Update Association.java
    added association class

--- GroupProjectOOP/src/groupWork/Association.java
+++ GroupProjectOOP/src/groupWork/Association.java
@@ -1,5 +1,73 @@
 package groupWork;
 
+import java.io.*;
+import java.util.*;
+
 public class Association {
+	private int idNum;
+	private String address;
+	private int numOfClubs;
+	
+	//methods
+	public void addAssociation(int idNum, String address, int numOfClubs)
+	{
+		this.idNum = idNum;
+		this.address = address;
+		this.numOfClubs = numOfClubs;
+		
+		try {
+			FileWriter AssoFile = new FileWriter("AssociationList.txt" , true);
+			AssoFile.write(idNum + " ");
+			AssoFile.write(address + " ");
+			AssoFile.write(numOfClubs + "");
+			AssoFile.write("\n");
+			AssoFile.close();
+
+			
+		}
+		catch(IOException e)
+		{
+			System.out.println("Error.This record could not be added to the file");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void ViewAssociation()
+	{		
+		idNum = 0;
+		address = "";
+		numOfClubs = 0;
+		try {
+			Scanner FileIn = new Scanner(new File("AssociationList.txt"));
+			while(FileIn.hasNext())
+			{
+				idNum = FileIn.nextInt();
+				address = FileIn.next();
+				numOfClubs = FileIn.nextInt();
+				
+				System.out.println("ID Number" + idNum);
+				System.out.println("Address" + address);
+				System.out.println("Number of Clubs in association" + numOfClubs);
+
+				
+			}
+			
+		}catch(FileNotFoundException e)
+		{
+			System.out.println("Error. User information could not be retrieved.");
+			e.printStackTrace();
+		}
+	
+	}
+	
+	public void DeleteAssociation()
+	{
+		
+	}
+	
+	
+	
+	
 
 }
