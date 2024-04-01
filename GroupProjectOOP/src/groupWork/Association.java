b703995ebf9146a46e8d59fa7611f95d651dfd4d
oc <oconnorburton387@gmail.com>
2024-04-01 13:42:52 -0500

    Update Association.java

--- GroupProjectOOP/src/groupWork/Association.java
+++ GroupProjectOOP/src/groupWork/Association.java
@@ -9,6 +9,13 @@ public class Association {
 	private int numOfClubs;
 	//private Club club;
 	
+	public Association()
+	{
+		idNum = 0;
+		address = " ";
+		numOfClubs = 0;
+	}
+	
 	//primary constructor
 	public Association(int idNum, String address, int numOfClubs)
 	{
@@ -17,14 +24,33 @@ public class Association {
 		this.numOfClubs = numOfClubs;
 	}
 	
+	public int GetIdNum() {
+		return idNum;
+	}
+
+	public void SetIdNum(int idNum) {
+		this.idNum = idNum;
+	}
+
+	public String GetAddress() {
+		return address;
+	}
+
+	public void SetAddress(String address) {
+		this.address = address;
+	}
+
+	public int GetNumOfClubs() {
+		return numOfClubs;
+	}
+
+	public void SetNumOfClubs(int numOfClubs) {
+		this.numOfClubs = numOfClubs;
+	}
+
 	//methods
 	public void addAssociation()
 	{
-		/*
-		this.idNum = idNum;
-		this.address = address;
-		this.club = club;
-		*/
 		
 		try {
 			int id;
@@ -148,7 +174,38 @@ public class Association {
 			e.printStackTrace();
 		}
 		
+		/*
+		File dFile = new File("AssociationList.txt");
+		dFile.delete();
 		
+		try {
+				Scanner inFile = new Scanner(new File("tempAssociation.txt"));
+				while(inFile.hasNext())
+				{
+					int main1 = inFile.nextInt();
+					String main3 = inFile.next();
+					int main2 = inFile.nextInt();
+					
+					try {
+					FileWriter nFile = new FileWriter("AssociationList.txt", true);
+						nFile.write(main1 + " ");
+						nFile.write(main2 + " ");
+						nFile.write(main3 + " ");
+						nFile.close();
+
+					}
+					catch(IOException e) {
+						
+					}
+				}
+				inFile.close();
+				
+
+			}
+			catch(FileNotFoundException ex) {
+				
+			}
+	*/
 		
 		
 	}
@@ -161,6 +218,66 @@ public class Association {
 	
 	public void SingleReport()
 	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the id number for the association that you want the report on: ");
+		int id = in.nextInt();
+		
+		try {
+			Scanner inFile = new Scanner(new File("AssociationList.txt"));
+			while(inFile.hasNext())
+			{
+				int idN = inFile.nextInt();
+				String addr = inFile.next();
+				int noc = inFile.nextInt();
+				
+				if(id == idN) {
+					idNum = idN;
+					address = addr;
+					numOfClubs = noc;
+					break;
+				}
+			}
+			
+			inFile.close();
+		
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("Error. Record not found");
+			e.printStackTrace();
+		}
+		
+		System.out.println("Association ID Number: " + idNum);
+		System.out.println("Association Address: " + address);
+		System.out.println("Number of Clubs in association: " + numOfClubs);
+		
+		try {
+			Scanner iniFile = new Scanner(new File("ClubList.txt"));
+			while(iniFile.hasNext())
+			{
+				int cc = iniFile.nextInt();
+				String cn = iniFile.next();
+				String colour = iniFile.next();
+				int members = iniFile.nextInt();
+				String pres = iniFile.next();
+				int idN = iniFile.nextInt();
+				String addr = iniFile.next();
+				int noc = iniFile.nextInt();
+				
+				if(id == idN) {
+					System.out.println("Club Name: " + cn);
+					System.out.println("Club Code: " + cc);
+					System.out.println("Club colour: " + colour);
+					System.out.println("Club president: " + pres + " \n");
+				}
+			}
+			
+			iniFile.close();
+		
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("Error. Record not found");
+			e.printStackTrace();
+		}
 		
 	}
 	
