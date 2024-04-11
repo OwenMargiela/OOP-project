3164a7aabc93eb5cfab38f08cea38e7cefe77b92
Oconnor Burton <112232447+merratio@users.noreply.github.com>
2024-04-11 09:41:31 -0500

    Update Sponsorship.java

--- GroupProjectOOP/src/groupWork/Sponsorship.java
+++ GroupProjectOOP/src/groupWork/Sponsorship.java
@@ -1,11 +1,22 @@
 package groupWork;
 
-public class Sponsorship {
+import java.util.*;
+import java.io.*;
+
+public class Sponsor {
     private String sponsorName;
     private String athleteName;
     private double amount;
+   
+    //default constructor
+    public Sponsor() {
+        sponsorName = "";
+        athleteName = "";
+        amount = 0;
+    }
 
-    public Sponsorship(String sponsorName, String athleteName, double amount) {
+    //primary constructor
+    public Sponsor(String sponsorName, String athleteName, double amount) {
         this.sponsorName = sponsorName;
         this.athleteName = athleteName;
         this.amount = amount;
@@ -35,11 +46,35 @@ public class Sponsorship {
     public void setAmount(double amount) {
         this.amount = amount;
     }
+    
+    //add sponsorship
+    public void AddSponsor()
+    {
+    	Scanner in = new Scanner(System.in);
+    	System.out.println("Enter the name of the Sponsor: ");
+    	String name = in.nextLine();
+    	
+    	System.out.println("Enter the name of the athlete associated with the Sponsor: ");
+    	String athName = in.nextLine();
+    	
+    	System.out.println("Enter the amount of money that the Sponsor covers: ");
+    	double money = in.nextDouble();
+    	
+    	//opening file 
+    	try {
+    		FileWriter file = new FileWriter("SponsorList.txt", true);
+    		file.write(name + " ");
+    		file.write(athName + " ");
+    		file.write(money + " ");
+    		file.close();
 
-    // Method to print details of the sponsorship
-    public void printDetails() {
-        System.out.println("Sponsor Name: " + sponsorName);
-        System.out.println("Athlete Name: " + athleteName);
-        System.out.println("Amount: $" + amount);
+    	}
+    	catch(IOException e) {
+    		e.printStackTrace();
+    	}
+
+    	
     }
+
+   
 }
